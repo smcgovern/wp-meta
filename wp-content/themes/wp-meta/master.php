f70876bcf0f01a0ac9188badd178c430fe564b9a
smcgovern <smcgovern@sidesmedia.com>
2015-05-09 00:56:18 -0400

    minor update

---
+++ wp-content/themes/wp-meta/master.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>{{ get_bloginfo('name') }}</title>
+
+	<link href="{{ get_template_directory_uri() }}/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ get_template_directory_uri() }}/assets/css/main.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="{{ get_template_directory_uri() }}/assets/js/bootstrap.min.js"></script>
+	<script src="{{ get_template_directory_uri() }}/assets/js/main.js"></script>
+</body>
+</html>
