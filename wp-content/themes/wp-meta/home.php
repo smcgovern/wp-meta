b1f53799bcb2fee2fb00235bb0f8e0335aeb3ebc
smcgovern <smcgovern@sidesmedia.com>
2015-05-09 13:22:42 -0400

    acf plugin

--- wp-content/themes/wp-meta/home.php
+++ wp-content/themes/wp-meta/home.php
@@ -1,9 +1,11 @@
 @layout('master')
 
 @section('content')
-	@wpposts
-		<a href="{{ the_permalink() }}">{{ the_title() }}</a><br>
-	@wpempty
-		<p>404</p>
-	@wpend
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		@wpposts
+			<a href="{{ the_permalink() }}">{{ the_title() }}</a><br>
+		@wpempty
+			<p>404</p>
+		@wpend
+	</div><!-- ./col-lg-12 -->
 @endsection
